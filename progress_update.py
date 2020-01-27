16bc2dab0b91681768538d22b5c86d5cb4d83528
Huihuo Zheng <zhenghh04@gmail.com>
2020-01-26 23:57:32 -0600

    fixed some bugs

--- progress_update.py
+++ progress_update.py
@@ -14,16 +14,18 @@ class Progress:
         self.readers=[]
         self.sh = None
         self.sheet=fstr
+        self.tt = None
         if fstr!='':
             self.getRemote(fstr)
-    def getNumPages(self, name="", t=["11/01/2019", "12/01/2019"]):
-        dat = self.data[self.data["您的姓名"]==name]
-        dat=dat[dat.Timestamp>t[0]]
-        dat=dat[dat.Timestamp<t[1]]
+    def getNumPages(self, name="", t=["2019/11/01", "2019/12/01"]):
         p = 0
-        for rec in dat["文集页数 [如: 20-100]"].values:
-            tmp = rec.split('-')
-            p += int(tmp[1]) - int(tmp[0])+1
+        n = len(self.data)
+        i = 0
+        for rec in self.data["文集页数 [如: 20-100]"].values:
+            if (not self.date.values[i]>t[1]) and (not self.date.values[i]<t[0]) and self.data["您的姓名"][i]==name:
+                tmp = rec.split('-')
+                p += int(tmp[1]) - int(tmp[0])+1
+            i+=1
         return p
     def getChurch(self):
         self.church={}
@@ -33,6 +35,14 @@ class Progress:
         return self.church
     def loadCSV(self, fstr):
         self.data=pd.read_csv(fstr)
+        n = len(self.data)
+        t={}
+        t["date"] = []
+        for i in range(n):
+            m, d, y = self.data.Timestamp[i].split()[0].split("/")
+            t["date"].append("%s/%s/%s" %(y, m, d))
+        self.date = pd.DataFrame(data=t)
+        
     def getReaders(self):
         self.readers=list(set([n.strip() for n in self.data["您的姓名"].values]))
         self.num_readers = len(self.readers)
@@ -48,17 +58,15 @@ class Progress:
         list_of_lists = worksheet.get_all_values()
         headers=list_of_lists[0]
         self.data = pd.DataFrame(list_of_lists[1:], columns=headers)
-    def getProgress(self, select="month", verbose=1):
+        n = len(self.data)
+        t={}
+        t["date"] = []
+        for i in range(n):
+            m, d, y = self.data.Timestamp[i].split()[0].split("/")
+            t["date"].append("%s/%s/%s" %(y, m, d))
+        self.date = pd.DataFrame(data=t)
+    def getProgress(self, t, verbose=0):
         record={}
-        today = datetime.today()
-        if (select=="month"):
-            t="%s/01/%s-%s/01/%s"%(today.month, today.year, today.month+1, today.year)
-        elif (select=="year"):
-            t="01/01/%s-12/31/%s"%(today.year, today.year)
-        elif (select=="all"):
-            t="01/01/0000-12/31/3100"
-        else:
-            t=select
         t=t.split('-')
         if (verbose==1):
             print("时间：%s - %s"%(t[0], t[1]))
@@ -70,21 +78,21 @@ class Progress:
                 print("%3s %5d"%(user, p))
             record[user]=p
         return record
-    def reportProgress(self):
+    def reportProgress(self, y=0):
         self.getRemote(self.sheet)
         record = {}
         today = datetime.today()
         readers = sorted(self.getReaders())
-
         for u in readers:
             record[u]=[]
-        y = today.year
+        if (y==0):
+            y = today.year
         for m in range(1,13):
-            t="%02d/01/%s-%02d/01/%s"%(m, y, m+1, y)
-            r = self.getProgress(select=t, verbose=0)
+            t="%s/%d/1-%s/%d/31"%(y, m, y, m)
+            r = self.getProgress(t, verbose=0)
             for u in readers:
                 record[u].append(r[u])
-        rt = self.getProgress(select="all", verbose=0)
+        rt = self.getProgress(t="0000/0/0-2200/0/0", verbose=0)
         try:
             worksheet = self.sh.worksheet("%s年每月进度统计"%y)
         except:
@@ -108,7 +116,7 @@ class Progress:
             worksheet.update_cell(r, 17, u)
             for i in range(12):
                 if (record[u][i]!=0):
-                     worksheet.update_cell(r, 2+i, record[u][i])
+                     worksheet.update_cell(r, 3+i, record[u][i])
             worksheet.update_cell(r, 15, sum(record[u][:12]))
             worksheet.update_cell(r, 16, rt[u])
             r=r+1
@@ -117,6 +125,7 @@ class Progress:
     
 def main():
     rp = Progress("cwwl-midwest-reading-progress")
+    #rp.reportProgress(y=2019)
     rp.reportProgress()
 
 if __name__=="__main__":
